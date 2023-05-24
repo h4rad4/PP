d6710c4074c59aae879e5c6b75912bc7b03261b9
h4rad4 <99924347+h4rad4@users.noreply.github.com>
2023-05-24 13:59:38 -0400

    Update Main.java

--- ex03/src/Main.java
+++ ex03/src/Main.java
@@ -12,25 +12,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        System.out.println("Qual operação deseja realizar? \n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão \n 5 - Tabuada");
+        System.out.println("\n Qual operação deseja realizar? \n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão \n 5 - Tabuada");
         int option = teclado.nextInt();
 
         switch (option) {
             case 1:
                 System.out.printf("Soma = %d", soma());
+                main(null);
                 break;
             case 2:
                 System.out.printf("Subtração = %d", subtracao());
+                main(null);
                 break;
             case 3:
                 System.out.printf("Multiplicação = %d", multiplicacao());
+                main(null);
                 break;
             case 4:
                 System.out.printf("Divisão = %.2f", divisao());
+                main(null);
                 break;
             case 5:
-                System.out.println("-------- Tabuada -------- \n");
+                System.out.println("-------- [Tabuada] -------- \n");
                 tabuada();
+                main(null);
                 break;
         }
     }
@@ -98,9 +103,31 @@ public class Main {
         System.out.println("Insira o valor. ");
         int v1 = teclado.nextInt();
 
-        for (int i = 1; i < 10; i++)
-        {
-            System.out.printf("%d * %d = %d \n", v1, i, v1*i);
+        System.out.println("Escolha a operação: 1- (+), 2- (-), 3- (*), 4- (:)");
+        int option = teclado.nextInt();
+
+        if (option == 1) {
+            for (int i = 1; i < 10; i++) {
+                System.out.printf("%d + %d = %d \n", v1, i, v1 + i);
+            }
+        }
+
+        if (option == 2) {
+            for (int i = 1; i < 10; i++) {
+                System.out.printf("%d - %d = %d", v1, i, v1 - i);
+            }
+        }
+
+        if (option == 3) {
+            for (int i = 1; i < 10; i++) {
+                System.out.printf("%d * %d = %d", v1, i, v1 * i);
+            }
+        }
+
+        if (option == 4) {
+            for (int i = 1; i < 10; i++) {
+                System.out.printf("%d : %d = %d", v1, i, v1 / i);
+            }
         }
     }
 }
